69c8fdb62df0eb3f38d597ab8bb95d0acd6dd17e
petru <george.petrusel@stud.ubbcluj.ro>
2023-04-04 15:48:45 +0300

    day 4

---
+++ src/main/java/com/appsdeveloper/app/ws/mobileappws/ui/model/response/OperationStatusModel.java
@@ -0,0 +1,12 @@
+package com.appsdeveloper.app.ws.mobileappws.ui.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OperationStatusModel {
+    private String operationResult;
+    private String operetionName;
+
+}
